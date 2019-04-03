ba106d7d64ad29ad8765e5517e3d18155a38d963
SnowzTail <i@snowztail.com>
2019-04-03 21:27:49 +0100

    🔧 Use Hamming window and zoom in to the interest

--- part1/task1_2_b.m
+++ part1/task1_2_b.m
@@ -6,9 +6,9 @@ fSample = eeg.fs;
 % length of signal
 nSamples = length(eeg.POz);
 % Hertz per sample
-fResolution = 1 / 5;
+fRes = 1 / 5;
 % FFT points
-nFft = fSample / fResolution;
+nFft = fSample / fRes;
 % duration of window
 tWindow = [10 5 1];
 % remove mean
@@ -18,34 +18,37 @@ nOverlap = 0;
 % declare vars
 psdAvg = cell(length(tWindow), 1);
 %% Standard periodogram
-[psdStd, fAnalog] = periodogram(poz, rectwin(nSamples), nFft, fSample);
+[psdStd, f] = periodogram(poz, hamming(nSamples), nFft, fSample);
 %% Bartlett periodogram: averaging with rectangular windows
 for iWindow = 1: length(tWindow)
     % number of samples of windows
     nWindows = tWindow(iWindow) * fSample;
-    psdAvg{iWindow} = pwelch(poz, rectwin(nWindows), nOverlap, nFft, fSample);
+    psdAvg{iWindow} = pwelch(poz, hamming(nWindows), nOverlap, nFft, fSample);
 end
 %% Result Plots
 % standard
 figure;
 subplot(2, 1, 1);
-plot(fAnalog, pow2db(psdStd), 'k');
+plot(f, pow2db(psdStd), 'k', 'LineWidth', 2);
 grid on; grid minor;
 legend('Standard');
 title('Periodogram of EEG: standard method');
 xlabel('Frequency (Hz)');
-ylabel('Power spectral density (dB/Hz)');
+ylabel('PSD (dB)');
+xlim([0 60]);
 ylim([-150 -90]);
 % Bartlett
 subplot(2, 1, 2);
 legendStr = cell(length(tWindow), 1);
 for iWindow = 1: length(tWindow)
-    plot(fAnalog, pow2db(psdAvg{iWindow}));
-    legendStr{iWindow} = sprintf('Window length = %d sec', tWindow(iWindow));
+    plot(f, pow2db(psdAvg{iWindow}), 'LineWidth', 2);
+    legendStr{iWindow} = sprintf('\\Deltat = %d sec', tWindow(iWindow));
     hold on;
 end
 grid on; grid minor;
 legend(legendStr);
 title('Periodogram of EEG: Bartlett method');
 xlabel('Frequency (Hz)');
-ylabel('Power spectral density (dB/Hz)');
+ylabel('PSD (dB)');
+xlim([0 60]);
+ylim([-150 -90]);
